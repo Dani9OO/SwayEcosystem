f28cd73ba5d1121bd8acc846abff2df031970fba
dani9oo <dani9oo.dev@gmail.com>
2020-01-29 12:16:22 -0600

    Now using wpg instead of wal

--- UserSetup.sh
+++ UserSetup.sh
@@ -1,18 +1,24 @@
 echo 'Updating user directories so the copy goes smooth.'
 xdg-user-dirs-update
 echo 'Copying user configuration files to your user folder.'
-cp -r home/cyberrumor/* ~/
-cp -r home/cyberrumor/.* ~/
+cp -r home/dani9oo/* ~/
+cp -r home/dani9oo/.* ~/
 echo 'Cleaning up unused files'
 rm ~/makodemo.png
 rm ~/blurshot.png
 rm ~/waldemo.png
 chmod +x ~/.config/mako/launch.sh
-echo 'Setting wallpaper with wal'
-wal -i /usr/share/backgrounds/archlinux/archlinux-burn.jpg
+echo 'Installing wpg components for window and icon theming'
+touch ~/.Xresources
+mkdir ~/.themes
+wpg-install.sh -g -i
+echo 'Setting preview wallpaper with wpg'
+wpg -a /usr/share/backgrounds/archlinux/archlinux-burn.jpg
+wpg -s archlinux-burn.jpg
 ln -s ~/.cache/wal/mako.conf ~/.config/mako/config
-echo 'To change your wallpaper, and theme your system dynamically, run
-	$ wal -i /path/to/image.jpg'
-echo 'For waybar icons to display, you must install
-	the git version of material design icons from here:
-	https://aur.archlinux.org/ttf-material-design-icons-git/'
+echo 'To add a wallpaper to your collection, run 
+	$ wpg -a /path/to/image.png
+	To list the wallpapers available in your collection, use
+	$ wpg -l
+	To set your system theme and colorscheme run
+	$ wpg -s <wallpaper from the previous list>'
